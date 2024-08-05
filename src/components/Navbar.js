1beec03c075cd51dc5036c39ee340ad339285372
poojalahane <tekli@gmail.com>
2024-08-05 17:27:15 +0530

    fourth commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
     <>
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">$ <FaHome className="nav-icon" /></a>
+    <a className="navbar-brand" href="#"><h1>#.</h1><FaHome className="nav-icon" /></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
